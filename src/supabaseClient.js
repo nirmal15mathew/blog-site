a0b9c97c200464cf33c17e76149cb748c7ce4a4e
Tj <55346160+nirmal15mathew@users.noreply.github.com>
2021-08-03 20:58:50 +0530

    change

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "xxxx";
+const supabaseUrl = "https://vbdvzbwkruxheszrtins.supabase.co";
 const supabaseAnonKey =
-  "xxxxxx";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzIzMDUxNiwiZXhwIjoxOTQyODA2NTE2fQ.lnzEviyI-tuu1P_c-FsKAtlv0IkPRFkeQXetJ2DnWw0";
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export default supabase;
