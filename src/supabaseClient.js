d988aba347de95b05b8061db7590529d71a707aa
Tj <55346160+nirmal15mathew@users.noreply.github.com>
2021-08-07 18:08:44 +0530

    removed supabase install and added as cdn for removing the error while build compilation

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
-
 const supabaseUrl = "https://vbdvzbwkruxheszrtins.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzIzMDUxNiwiZXhwIjoxOTQyODA2NTE2fQ.lnzEviyI-tuu1P_c-FsKAtlv0IkPRFkeQXetJ2DnWw0";
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-export default supabase;
+const spb = supabase.createClient(supabaseUrl, supabaseAnonKey);
+export default spb;
